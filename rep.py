2b26e39ef3c4018da2a8e5dc4d211770efd35402
Jaime Bowen Varela <jbowvar@inta.es>
2022-06-14 14:44:24 +0200

    New add-ons

--- rep.py
+++ rep.py
@@ -2,14 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import plotly.graph_objects as go
+import plotly.express as px
 import numpy as np
+import streamlit as st
+import pandas as pd
 
+def plot_mesh(mesh_data:pd.DataFrame,y_true,y_pred):
+    """
+    Plot mesh with the values of CP
 
-def plot_mesh(mesh_data,y_true,y_pred):
+    :params mesh_data: coordiantes of the points of the mesh
+    :params y_true: true values of the CP 
+    :params y_pred: predicted values of the CP
+    """
     fig = go.Figure(data = [go.Mesh3d(x = mesh_data.X.values,
                                       y = mesh_data.Y.values,
                                       z = mesh_data.Z.values,
-                                      colorscale = [[0,"blue"],
+                                      colorscale = [[0,"white"],
                                                     [1,"red"]
                                       ],
                                       colorbar_title = "Error",
@@ -20,3 +29,6 @@ def plot_mesh(mesh_data,y_true,y_pred):
                                       )])
     return fig
 
+def plot_hist(data):
+    fig = px.bar(data, x = "dataset", y="r2",range_y= [0.85,1])
+    return fig
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,16 @@
 # streamlit_app.py
 
 import streamlit as st
+from PIL import Image
+from tacoma.metrics import GetScores
 import gdown
 import numpy as np
 from google.oauth2 import service_account
 from gsheetsdb import connect
 import pandas as pd
 from pathlib import Path
-from rep import plot_mesh
-
+from rep import plot_mesh,plot_hist
+from scipy.io import loadmat
 
 @st.cache(ttl=600)
 def run_query(query):
@@ -21,22 +23,38 @@ def run_query(query):
 def download_drive_dataset(url,fname):
     """
     Download .npz from Google Drive
+
+    :param url: url to the shared file
+    :param fname: name of the file
+
+    :returns path_to_file: Path to the file inside the system
     """
     download_path = Path("data_files")
     path_to_file = download_path/fname
     if not download_path.exists():
         download_path.mkdir()
-    if not  path_to_file.exists():
+    if not path_to_file.exists():
         gdown.download(url,output = str(path_to_file),quiet = True,fuzzy = True)
     return path_to_file
 
-def npz_to_df(data):
-    case = range(1,len(data["X"])+1)
+def npz_to_df(path:Path):
+  
+    try:
+        data = loadmat(str(path)+".mat")
+        x_name = "X_test"
+    except:
+        try:
+            data = np.load(str(path)+".npz",allow_pickle=True)
+            x_name = "X"
+        except:
+            raise ValueError(f"Data format{path} not supported")
+
+    case = range(1,len(data[x_name])+1)
     df = pd.DataFrame()
     df["CaseID"] = case
-    df["Mach"] = data["X"][:,0]
-    df["Alpha"] = data["X"][:,1]
-    return df
+    df["Mach"] = data[x_name][:,0]
+    df["Alpha"] = data[x_name][:,1]
+    return df,data
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
@@ -49,20 +67,35 @@ conn = connect(credentials=credentials)
 # Uses st.cache to only rerun when the query changes or after 10 min.
 sheet_url = st.secrets["private_gsheets_url"]
 ### DATA LOADING
-st.title("ICAS 2022")
+st.title("ICAS 2022") ## Titulo principal 
+st.image(Image.open("assets/imgs/icas_logo.png"))
 choices_query = run_query(f'SELECT * FROM "{sheet_url}"')
 choices_query = pd.DataFrame(choices_query)
+st.table(choices_query.drop(columns = ["url"]))
+st.plotly_chart(plot_hist(choices_query.drop(columns = ["url"])))
 choices = st.multiselect("Dataset to show",choices_query.dataset.to_list())
 if not choices:
     st.stop()
 dataset = choices_query.loc[choices_query.dataset.isin(choices)]
-fname = Path(f"{choices[0]}.npz")
-fname = download_drive_dataset(dataset.url.values[0],fname = f"{choices[0]}.npz")
-data = np.load(fname,allow_pickle=True)
-st.write(data.files)
-df = npz_to_df(data)
+fname = download_drive_dataset(dataset.url.values[0],fname = f"{choices[0]}")
+df,data = npz_to_df(fname)
+if not fname.exists():
+    st.stop()
+
+
+
+##
+# TODO: añadir tablas de estadísiticas generales
+general_scores = GetScores(data["y_true"],data["y_pred"]).get_errors().mean()
+r2_col,rmse_col,me_col = st.columns(3)
+r2_col.metric("R2",round(general_scores.r2,2))
+rmse_col.metric("Mean Squared Error",round(general_scores.mse,2))
+me_col.metric("Maximum Error",round(general_scores.me,2))
+
+## Elegir caso a visualizar
+
+
 mesh = pd.read_pickle("data_files/mesh_garteur.pkl")
-st.write(df)
 st.plotly_chart(plot_mesh(mesh,data["y_true"][0,:],data["y_pred"][0,:]))
 
 #### GENERAL STATS
